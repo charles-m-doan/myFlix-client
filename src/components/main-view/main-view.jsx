0e467ba44b8447e64deb615b42ce2f81084790f8
SiderFace <115373215+SiderFace@users.noreply.github.com>
2023-04-14 05:05:34 -0400

    3.6: First Submission

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,80 +1,121 @@
-import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
-import { SignupView } from "../signup-view/signup-view";
+import { useState, useEffect } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
   const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [user, setUser] = useState(undefined);
 
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
+   useEffect(() => {
+      if (!token) {
+         return;
+      }
 
-    fetch("https://siders-myflix.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMovies(data);
-      });
-  }, [token]);
+      fetch('https://siders-myflix.herokuapp.com/movies', {
+         headers: { Authorization: `Bearer ${token}` }
+      })
+         .then((response) => response.json())
+         .then((data) => {
+         console.log(data);
+         setMovies(data);
+         });
+   }, [token]);
+   
+   return (
+      <Row className='justify-content-md-center'>
+         {
+         !user ? (
+            < >
+               <Col md={3}>
+                  <LoginView
+                     onLoggedIn={(user, token) => {
+                     setUser(user);
+                     setToken(token);
+                     } }
+                  />
+                  <br /><br /><br />
+                  Or Sign Up here:
+                  <br /><br />
+                  <SignupView />
+               </Col>
+            </>
+         ) : 
+         selectedMovie ? (
+            <Col md={4}>
+               <MovieView
+                  movie={selectedMovie}
+                  onBackClick={() => setSelectedMovie(null)}
+               />
+            </Col>
+         ) : 
+         movies.length === 0 ? (
+            <div>The list is empty!</div>
+         ) : 
+         (
+            < >
+               {movies.map((movie) => (
+                  <Col 
+                     key={movie.id} 
+                     md={4}
+                     className='mb-5'
+                  >
+                     <MovieCard
+                        movie={movie}
+                        onMovieClick={(newSelectedMovie) => {
+                           setSelectedMovie(newSelectedMovie);
+                        }}
+                     />
+                  </Col>
+               ))}
+            </>
 
-  if (!user) {
-    return (
-      <>
-        <LoginView
-          onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-          }}
-        />
-        or
-        <SignupView />
-      </>
+         )}
+
+      </Row>
     );
   }
 
-  if (selectedMovie) {
-    return (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-    );
-  }
+//   if (selectedMovie) {
+//     return (
+//       <MovieView
+//         movie={selectedMovie}
+//         onBackClick={() => setSelectedMovie(null)}
+//       />
+//     );
+//   }
 
-  if (!movies || movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
+//   if (!movies || movies.length === 0) {
+//     return <div>The list is empty!</div>;
+//   }
 
-  return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-      <button
-        onClick={() => {
-          setUser(null);
-          setToken(null);
-          localStorage.clear();
-        }}
-      >
-        Logout
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {movies.map((movie) => (
+//         <MovieCard
+//           key={movie.id}
+//           movie={movie}
+//           onMovieClick={(newSelectedMovie) => {
+//             setSelectedMovie(newSelectedMovie);
+//           }}
+//         />
+//       ))}
+//       <button
+//         onClick={() => {
+//           setUser(null);
+//           setToken(null);
+//           localStorage.clear();
+//         }}
+//       >
+//         Logout
+//       </button>
+//     </div>
+//   );
+// };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,25 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import './movie-card.scss';
 
-export const MovieCard = ({ movie, onMovieClick }) => {
-  return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      <img src={movie.ImagePath} alt={"Poster for : " + movie.Title} /> ,
-      <h2> {movie.Title} </h2>
-      <div> {"Director: " + movie.Director.Name} </div>
-      <span>____________________________________</span>
-      <br /> <br /> <br />
-    </div>
-  );
-};
+export const MovieCard = ({ movie , onMovieClick }) => {
+   return (
+      <Card 
+         className="h-100"
+         onClick={() => onMovieClick(movie)}
+      >
+            <Card.Img variant='top' src={movie.ImagePath} />
+            <Card.Body>
+               <Card.Title>{movie.Title}</Card.Title>
+               <Card.Text>{movie.Director.Name}</Card.Text>
+               <Button onClick={() => onMovieClick(movie)} variant='link'>
+               Open
+               </Button>
+            </Card.Body>
+
+     </Card>
+   );
+ };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
